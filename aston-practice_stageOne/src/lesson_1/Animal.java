962519891d02928ecb75ae74d7fd37f22bc2fbc5
VlaDisLaW-SH <vlad.shulikoff@yandex.ru>
2024-05-16 20:52:10 +0700

    update Animal

--- aston-practice_stageOne/src/lesson_1/Animal.java
+++ aston-practice_stageOne/src/lesson_1/Animal.java
@@ -1,7 +1,53 @@
 package lesson_1;
 
-/**
- * todo Document type Animal
- */
 public class Animal {
+    private String name;
+    private Integer age;
+    private Integer weight;
+
+    public Animal(String name, Integer age, Integer weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Integer getAge() {
+        return this.age;
+    }
+
+    public Integer getWeight() {
+        return this.weight;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public void toEat() {
+        System.out.println(getName() + " is fed");
+    }
+
+    public void toEat(String product) {
+        System.out.println(getName() + " is fed of" + product);
+    }
+
+    public void toSleep() {
+        System.out.println(getName() + " is sleep");
+    }
+
+    public void toSleep(Integer time) {
+        System.out.println(getName() + " went to sleep in " + time);
+    }
 }
