5aa13928c3d69083a966004e21331f9e6bddbe6d
VlaDisLaW-SH <vlad.shulikoff@yandex.ru>
2024-05-19 23:11:19 +0700

    created Runner

---
+++ aston-practice_stageOne/src/lesson_2/task_2_2/Runner.java
@@ -0,0 +1,27 @@
+package lesson_2.task_2_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+        ImmutablePerson immutablePerson = new ImmutablePerson("James", 30, List.of("Fishing", "Collecting", "Golf"));
+
+        System.out.println(immutablePerson.getName());
+        System.out.println(immutablePerson.getHobbies());
+        System.out.println();
+        // изменить созданный объект immutablePerson нельзя (в доступных методах взаимодействия с данной сущьностью отсутсвуют Сеттеры)
+
+        MutablePerson mutablePerson = new MutablePerson("Jack", 31, new ArrayList<>());
+        System.out.println(mutablePerson.getName());
+        System.out.println(mutablePerson.getAge());
+        mutablePerson.addHobby("Hunting");
+        mutablePerson.addHobby("Collecting");
+        System.out.println(mutablePerson.getHobbies());
+
+        mutablePerson.setAge(33);
+        mutablePerson.addHobby("Diving");
+        System.out.println(mutablePerson.getAge());
+        System.out.println(mutablePerson.getHobbies());
+    }
+}
