df9087b7ca59a626a13d773459263e65cdcdc63a
VlaDisLaW-SH <vlad.shulikoff@yandex.ru>
2024-05-20 02:27:30 +0700

    fix ImmutablePerson

--- aston-practice_stageOne/src/lesson_2/task_2_2/ImmutablePerson.java
+++ aston-practice_stageOne/src/lesson_2/task_2_2/ImmutablePerson.java
@@ -2,13 +2,11 @@ package lesson_2.task_2_2;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 
 @AllArgsConstructor
 @Getter
-@Setter
 public final class ImmutablePerson {
 
     private final String name;
