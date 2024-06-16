1575a060ee67de619426457de4d669bb87a8b511
VlaDisLaW-SH <vlad.shulikoff@yandex.ru>
2024-06-17 04:01:39 +0700

    created MinskTimeServlet, WashingtonTimeServlet, BeijingTimeServlet

---
+++ aston-practice_stageOne/src/lesson_10/task_10_1/annotation/BeijingTimeServlet.java
@@ -0,0 +1,22 @@
+package lesson_10.task_10_1.annotation;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+@WebServlet("/beijing")
+public class BeijingTimeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of("Asia/Shanghai"));
+        String time = zonedDateTime.format(DateTimeFormatter.ISO_ZONED_DATE_TIME);
+        response.setContentType("text/plain; charset=UTF-8");
+        response.getWriter().write(time);
+    }
+}
